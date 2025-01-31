c019fb132e3cc8ba2c6ae5ec1707c270f9d068fc
Alex Volodin <alex-volodin@yandex.ru>
2025-01-31 11:33:57 +0300

    refactoring

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-this is RAG for gigachat
+Installation:
+
+1. Clone this repo into working folder
+
+2. Create Python virtual environment
+
+```
+python3 -m venv .venv
+```
+
+3. Activate virtual environment
+
+```
+source .venv/bin/activate
+```
+
+4. Install dependencies
+
+```
+pip install -r requirements.txt
+```
--- giga-rag.py
+++ giga-rag.py
@@ -3,32 +3,46 @@ from dotenv import load_dotenv
 import warnings
 from langchain.chains import RetrievalQA
 from langchain_chroma import Chroma
-from langchain_core.documents import Document
 from langchain_community.embeddings import SentenceTransformerEmbeddings
 from langchain_gigachat.chat_models import GigaChat
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 
 warnings.filterwarnings("ignore")
-load_dotenv()
 
-llm = GigaChat(
-    credentials=os.environ["auth_key"],
-    scope="GIGACHAT_API_PERS",
-    model="GigaChat",
-    streaming=False,
-    verify_ssl_certs=False
-)
+if __name__ == "__main__":
 
-loader = TextLoader("data/systems.txt")
-documents = loader.load()
+    question = "сколько внешних систем?"
 
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-docs = text_splitter.split_documents(documents)
-embeddings_function = SentenceTransformerEmbeddings(
-    model_name='all-MiniLM-L6-V2')
-db = Chroma.from_documents(docs, embeddings_function)
+    # load environment variables
+    load_dotenv()
 
-qa_chain = RetrievalQA.from_chain_type(llm, retriever=db.as_retriever())
-ans = qa_chain({"query": "сколько внешних систем?"})
-print(ans)
+    # initialize LLM object
+    llm = GigaChat(
+        credentials=os.environ["auth_key"],
+        scope="GIGACHAT_API_PERS",
+        model="GigaChat",
+        streaming=False,
+        verify_ssl_certs=False
+    )
+
+    # load text data from file
+    loader = TextLoader("data/systems.txt")
+    data = loader.load()
+
+    # split text into chunks
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    docs = text_splitter.split_documents(data)
+
+    # create embedings and put them into in-memory vector storage
+    db = Chroma.from_documents(docs, SentenceTransformerEmbeddings(
+        model_name='all-MiniLM-L6-V2'))
+
+    # prepare LLM to process request
+    qa_chain = RetrievalQA.from_chain_type(llm, retriever=db.as_retriever())
+
+    # request
+    ans = qa_chain({"query": question})
+
+    # response
+    print(ans)
