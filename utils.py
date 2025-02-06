cdecf318c1be45328e8b70ae725d39f3808d95b3
Alex Volodin <alex-volodin@yandex.ru>
2025-02-06 21:43:32 +0300

    added levels

--- utils.py
+++ utils.py
@@ -7,6 +7,14 @@ with open(filename, "r") as file, open("documents/standards.txt", "w") as out:
     arr = objects["seaf.change.requirements.dzo"]
     for k in arr:
         object = arr[k]
-        text = object["statement"]
+        text = object["statement"].strip()
+        sber = object["sber"]
+        levels = sber["applicability_level"]
+        if not text.endswith("."):
+            text = f"{text}."
         out.write(text)
+        out.write(
+            " Данное требования применяется для систем с уровнем критичности ")
+        out.write(",".join(levels))
+        out.write(".")
         out.write("\n")
